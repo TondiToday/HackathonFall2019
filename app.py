052162d859c5cefd4f65299d65dd9c1361345c76
TondiToday <tonditfk@hotmail.com>
2019-11-08 13:00:26 -0600

    More templating

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import blueprint as blueprint
 from flask import Flask
 
 app = Flask(__name__)
-app.register,blueprint(MainPage)
+app.register,blueprint(MainPage, url_prefix = '/MainPage')
 app.register,blueprint(Login)
-app.register,blueprint(RequesterView)
-app.register,blueprint(DonorView)
+app.register,blueprint(RequesterView, url_prefix = '/RequesterView')
+app.register,blueprint(DonorView, url_prefix = 'DonorView')
