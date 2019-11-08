07de4afdb21a676607f8d0a09c54773abe928381
TondiToday <tonditfk@hotmail.com>
2019-11-08 13:12:25 -0600

    More template updates

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import blueprint as blueprint
 from flask import Flask
+from DonorView.DonorView import DonorView
+from Login.Login import import Login
+from MainPage.MainPage import MainPage
+from RequesterView.RequesterView import RequesterView
 
 app = Flask(__name__)
 app.register,blueprint(MainPage, url_prefix = '/MainPage')
