e5cd1d7eeac48b41d6797ec348ecb13cd32ffac7
rmbgdf <rmbgdf@mail.umkc.edu>
2019-11-08 13:31:14 -0600

    Blarg

--- Login/Login.py
+++ Login/Login.py
@@ -1,5 +1,25 @@
 from flask import Flask, Blueprint
+from flask import Flask, Blueprint
+import json
+import os
+import sqlite3
 
+from flask import Flask, redirect, request, url_for
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_required,
+    login_user,
+    logout_user,
+)
+from oauthlib.oauth2 import WebApplicationClient
+import requests
+from db import init_db_command
+from user import User
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
 
 
 
